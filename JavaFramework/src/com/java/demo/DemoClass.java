1a15f7fced3782fe91363730b79a543d748ee82b
aakash2510 <aakashkumar2127@gmail.com>
2022-02-14 19:00:50 +0530

    initial commit

---
+++ JavaFramework/src/com/java/demo/DemoClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.java.demo;
+
+/**
+ * @author HP
+ *
+ */
+public class DemoClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
