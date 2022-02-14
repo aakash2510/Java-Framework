24c508940fb8ae8baeab74c6d62ff256a172de59
Aakash <62670959+aakash2510@users.noreply.github.com>
2022-02-14 19:31:19 +0530

    Update Identifiers.java
    added variable m

--- JavaFramework/src/com/java/fundamentals/Identifiers.java
+++ JavaFramework/src/com/java/fundamentals/Identifiers.java
@@ -12,6 +12,8 @@ public class Identifiers {
 	public static void main(String[] args) {
 		int j = 20;
 		System.out.println(j);
+		int m = 10;
+		System.out.println(m);
 		int i = 10;
 		System.out.println(i);
 		double k = 10.0;
