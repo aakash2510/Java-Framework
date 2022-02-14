51191b3fd8e1ef1c43d14024f5d453c08202005e
aakash2510 <aakashkumar2127@gmail.com>
2022-02-14 19:16:38 +0530

    modifying demoClass added operator package

---
+++ JavaFramework/src/com/java/fundamentals/Identifiers.java
@@ -0,0 +1,17 @@
+/**
+ * This class will explains the identifiers and keywords in java.
+ */
+package com.java.fundamentals;
+
+/**
+ * @author Aakash
+ *
+ */
+public class Identifiers {
+
+	public static void main(String[] args) {
+		int i = 10;
+		System.out.println(i);
+	}
+
+}
---
+++ JavaFramework/src/com/java/operators/Operators.java
@@ -0,0 +1,12 @@
+package com.java.operators;
+/**
+ * @author Aakash
+ *
+ */
+public class Operators {
+
+	public static void main(String[] args) {
+
+	}
+
+}
