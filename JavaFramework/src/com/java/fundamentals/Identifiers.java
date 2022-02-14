a0688eae35e110eca656cd86e5123620c011fc12
Aakash <62670959+aakash2510@users.noreply.github.com>
2022-02-14 19:26:52 +0530

    Update Identifiers.java
    added j and k variable

--- JavaFramework/src/com/java/fundamentals/Identifiers.java
+++ JavaFramework/src/com/java/fundamentals/Identifiers.java
@@ -10,8 +10,12 @@ package com.java.fundamentals;
 public class Identifiers {
 
 	public static void main(String[] args) {
+		int j = 20;
+		System.out.println(j);
 		int i = 10;
 		System.out.println(i);
+		double k = 10.0;
+		System.out.println(k);
 	}
 
 }
